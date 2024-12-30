9c1a0c268ec9bc74f54b0a807d33e58ea9cb78db
User <User@B01.r1805.tciii.org.tw>
2024-12-30 14:32:38 +0800

    12301432

---
+++ stest5/src/main/java/tw/brad/stest5/controller/T1Controller.java
@@ -0,0 +1,64 @@
+package tw.brad.stest5.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tw.brad.stest5.model.T1;
+import tw.brad.stest5.model.T2;
+import tw.brad.stest5.repository.T1Repository;
+
+@RequestMapping("/t1")
+@RestController
+public class T1Controller {
+
+	@Autowired
+	private T1Repository t1Repository;
+	
+	@GetMapping("/test1")
+	public void test1() {
+		T1 t1 = new T1();
+		t1.setF1("aaa2"); t1.setF2("bbb2");
+		
+		T2 t21 = new T2();
+		t21.setF3("ccc3"); t21.setF4("ddd3");
+		t1.addT2(t21);
+		
+		T2 t22 = new T2();
+		t22.setF3("ccc4"); t22.setF4("ddd4");
+		t1.addT2(t22);
+
+		T2 t23 = new T2();
+		t23.setF3("ccc5"); t23.setF4("ddd5");
+		t1.addT2(t23);
+		
+		t1Repository.save(t1);
+	}
+
+	
+	@GetMapping("/test2")
+	public void test2() {
+		Optional<T1> opt = t1Repository.findById(1L);
+		T1 t1 = opt.get();
+		System.out.println(t1.getF1());
+		System.out.println(t1.getF2());
+		List<T2> t2s = t1.getT2s();
+		for (T2 t2: t2s) {
+			System.out.printf("%s : %s\n", t2.getF3(), t2.getF4());
+		}
+		
+	}
+	
+	@GetMapping("/test3")
+	public void test3() {
+		Optional<T1> opt = t1Repository.findById(1L);
+		T1 t1 = opt.get();
+		t1Repository.delete(t1);
+		
+	}
+	
+}
